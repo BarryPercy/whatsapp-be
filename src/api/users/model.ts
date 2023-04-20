580c0b788260180245bac916c3d5bac0e306c42d
MariusAuerbacher <101823468+MariusAuerbacher@users.noreply.github.com>
2023-04-20 17:59:27 +0200

    default avatar

--- src/api/users/model.ts
+++ src/api/users/model.ts
@@ -8,60 +8,68 @@ export interface UserDocument extends Document {
   password: string;
   avatar?: string;
   status?: string;
-  role: 'User' | 'Admin';
+  role: "User" | "Admin";
   accessToken?: string;
 }
 
 interface UserModelInterface extends Model<UserDocument> {
-  checkCredentials(email: string, plainPW: string): Promise<UserDocument | null>;
+  checkCredentials(
+    email: string,
+    plainPW: string
+  ): Promise<UserDocument | null>;
 }
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    avatar: { type: String, default: `http://placekitten.com/200/300` },
+    avatar: {
+      type: String,
+      default: `https://www.maxpixel.net/static/photo/1x/Profile-Man-Symbol-Human-Communication-User-Home-42914.png `,
+    },
     status: { type: String },
     role: { type: String, enum: ["User", "Admin"], default: "User" },
     accessToken: { type: String },
-  }, {timestamps: true});
-  
-UserSchema.pre("save", async function() {
-    const newUserData = this
-    if (newUserData.isModified("password")) {
-        const plainPw = newUserData.password
-        const hash = await bcrypt.hash(plainPw, 16)
-        newUserData.password = hash
-    }
-})
+  },
+  { timestamps: true }
+);
 
-UserSchema.methods.toJSON = function() {
-    const currentUser = this.toObject()
-    delete currentUser.password
-    delete currentUser.createdAt
-    delete currentUser.updatedAt
-    delete currentUser.__v
+UserSchema.pre("save", async function () {
+  const newUserData = this;
+  if (newUserData.isModified("password")) {
+    const plainPw = newUserData.password;
+    const hash = await bcrypt.hash(plainPw, 16);
+    newUserData.password = hash;
+  }
+});
 
-    return currentUser
-}
+UserSchema.methods.toJSON = function () {
+  const currentUser = this.toObject();
+  delete currentUser.password;
+  delete currentUser.createdAt;
+  delete currentUser.updatedAt;
+  delete currentUser.__v;
 
-
-
+  return currentUser;
+};
 
 UserSchema.static("checkCredentials", async function (email, plainPW) {
-    const user = await this.findOne({ email })
-    if (user) {
-      const passwordMatch = await bcrypt.compare(plainPW, user.password)
-      if (passwordMatch) {
-        return user
-      } else {
-        return null
-      }
+  const user = await this.findOne({ email });
+  if (user) {
+    const passwordMatch = await bcrypt.compare(plainPW, user.password);
+    if (passwordMatch) {
+      return user;
     } else {
-    
-      return null
+      return null;
     }
-  })
-  const UserModel: UserModelInterface = model<UserDocument, UserModelInterface>("User", UserSchema);
+  } else {
+    return null;
+  }
+});
+const UserModel: UserModelInterface = model<UserDocument, UserModelInterface>(
+  "User",
+  UserSchema
+);
 
 export default UserModel;
